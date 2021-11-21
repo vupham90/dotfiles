ee61df348e9a6dd6d4ac7d7e7bd8b3db1d888fb2
Vu Pham <dinhvu.pham@shopee.com>
2021-11-21 16:20:07 +0800

    move file & add tmux

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ brew update
 
 brew install alacritty ripgrep 
 
-ln -s $(pwd)/zsh/.zshrc ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 ln -s $(pwd)/nvim ~/.config/nvim
 ln -s $(pwd)/alacritty ~/.config/alacritty
